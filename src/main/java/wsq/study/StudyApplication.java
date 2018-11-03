7f0e83c075799f625a17e4c10db14769f9935460
weisq <996147529@qq.com>
2018-11-03 21:04:59 +0800

    创建学习的springboot项目

---
+++ src/main/java/wsq/study/StudyApplication.java
@@ -0,0 +1,12 @@
+package wsq.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApplication.class, args);
+	}
+}
