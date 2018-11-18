756274dbb3912741c2a68860130784f201e45cd8
weisq <996147529>
2018-11-18 18:31:20 +0800

    启动类静态代码块引入自定义全局配置文件

---
+++ src/main/java/wsq/study/Application.java
@@ -0,0 +1,20 @@
+package wsq.study;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication(scanBasePackages = {"wsq.study.*"})
+@EnableTransactionManagement
+@MapperScan({"wsq.study.mapper"})
+public class Application {
+    static
+    {
+        //引入配置中心文件
+        System.setProperty("spring.config.name", "wsq");
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
